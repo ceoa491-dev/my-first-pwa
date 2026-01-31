cc909ba3029a682f5e6721f57531bb23757c26c3
Dinesh <ceoa491@gmail.com>
2026-01-31 21:11:20 +0530

    Updated PWA changes

--- sw.js
+++ sw.js
@@ -1,19 +1,37 @@
-self.addEventListener("install",event=>{
-    event.waitUntil(
-        caches.open("pwa-cache-v3").then(cache=>{
-            return cache.addAll([
-                "./",
-                "index.html",
-                "app.js",
-                "manifest.json"
-            ]);
-        })
-    );
+const CACHE_NAME = "pwa-cache-v3";
+
+self.addEventListener("install", event => {
+  self.skipWaiting();
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache =>
+      cache.addAll([
+        "./",
+        "./index.html",
+        "./style.css",
+        "./app.js",
+        "./manifest.json"
+      ])
+    )
+  );
 });
-self.addEventListener("fetch",event=>{
-    event.respondWith(
-        caches.match(event.request).then(response=>{
-            return response || fetch(event.request);
+
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(
+        keys.map(key => {
+          if (key !== CACHE_NAME) {
+            return caches.delete(key);
+          }
         })
-    );
+      )
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    fetch(event.request).catch(() => caches.match(event.request))
+  );
 });
