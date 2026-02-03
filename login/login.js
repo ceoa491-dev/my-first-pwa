087e860489e61df621acf301a90bfeb3c84dade6
Dinesh <ceoa491@gmail.com>
2026-02-03 19:01:09 +0530

    Updated PWA changes

---
+++ login/login.js
@@ -0,0 +1,23 @@
+document.getElementById("childsafell").addEventListener("submit",function(e){
+    e.preventDefault();
+    const data={
+        name:document.getElementById("name").value,
+        email:document.getElementById("email").value,
+        passs:document.getElementById("pass").value,
+        who:document.getElementById("who").value
+    };
+    fetch("https://childsafety-backend-6.onrender.com/child/create",{
+        method: "POST",
+        headers:{
+            "Content-Type":"application/json"
+        },
+        body:JSON.stringify(data)
+    })
+    .then(res=>res.json())
+    .then(res=>{
+        alert("Data Saved Successfully");
+        
+        console.log(res);
+    })
+    .catch(err=>console.log(err));
+});
